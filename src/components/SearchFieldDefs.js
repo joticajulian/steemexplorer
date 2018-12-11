9dc810abf2e294eea5d58dfe01a724e9305645fe
danlex <dan_lex@example.com>
2018-12-11 08:47:38 +0100

    Search Page added vuedatatable

---
+++ src/components/SearchFieldDefs.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    name: '__handle',
+    titleClass: 'center aligned',
+    dataClass: 'center aligned'
+  },
+  {
+    name: '__sequence',
+    title: '#',
+    titleClass: 'center aligned',
+    dataClass: 'right aligned'
+  },
+  {
+    name: '__checkbox',
+    titleClass: 'center aligned',
+    dataClass: 'center aligned'
+  },
+  {
+    name: 'issuerName',
+    title: 'Issuer Name',
+    sortField: 'issuerName',
+  }, 
+  {
+    name: 'homeMemberState',
+    title: 'HBS',
+    sortField: 'homeMemberState'
+  }, 
+  {
+    name: 'language',
+    sortField: 'language'
+  },
+  {
+    name: 'docDisclosureDate',
+    title: 'Disclosure date',
+    sortField: 'docDisclosureDate',
+  },
+  {
+    name: 'title',
+    sortField: 'title',
+  },
+  {
+    name: '__slot:actions',
+    title: 'Actions',
+    titleClass: 'center aligned',
+    dataClass: 'center aligned',
+  }
+]
---
+++ src/components/SearchVuetable.vue
@@ -0,0 +1,145 @@
+<script>
+import accounting from 'accounting';
+import moment from 'moment';
+import Vue from 'vue';
+import VueEvents from 'vue-events';
+import Vuetable from 'vuetable-2/src/components/Vuetable';
+import VuetablePagination from 'vuetable-2/src/components/VuetablePagination';
+import VuetablePaginationInfo from 'vuetable-2/src/components/VuetablePaginationInfo';
+import CustomActions from './SearchCustomActions';
+import FilterBar from './searchFilterBar';
+
+Vue.use(VueEvents);
+Vue.component('custom-actions', CustomActions);
+Vue.component('filter-bar', FilterBar);
+
+export default {
+  components: {
+    Vuetable,
+    VuetablePagination,
+    VuetablePaginationInfo
+  },
+  props: {
+    apiUrl: {
+      type: String,
+      required: true
+    },
+    fields: {
+      type: Array,
+      required: true
+    },
+    sortOrder: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    appendParams: {
+      type: Object,
+      default() {
+        return {}
+      }
+    },
+    detailRowComponent: {
+      type: String
+    }
+  },
+  data () {
+    return {}
+  },
+  mounted () {
+    this.$events.$on('filter-set', eventData => this.onFilterSet(eventData))
+    this.$events.$on('filter-reset', e => this.onFilterReset())
+  },
+  render(h) {
+    return h(
+      'div', 
+      {
+        class: { ui: true, container: true }
+      },
+      [
+        h('filter-bar'),
+        this.renderVuetable(h),
+        this.renderPagination(h)
+      ]
+    )
+  },
+  methods: {
+    // render related functions
+    renderVuetable(h) {
+      return h(
+        'vuetable', 
+        { 
+          ref: 'vuetable',
+          props: {
+            apiUrl: this.apiUrl,
+            fields: this.fields,
+            paginationPath: "",
+            perPage: 10,
+            multiSort: true,
+            sortOrder: this.sortOrder,
+            appendParams: this.appendParams,
+            detailRowComponent: this.detailRowComponent,
+          },
+          on: {
+            'vuetable:cell-clicked': this.onCellClicked,
+            'vuetable:pagination-data': this.onPaginationData,
+          },
+          scopedSlots: this.$vnode.data.scopedSlots
+        }
+      )
+    },
+    renderPagination(h) {
+      return h(
+        'div',
+        { class: {'vuetable-pagination': true, 'ui': true, 'basic': true, 'segment': true, 'grid': true} },
+        [
+          h('vuetable-pagination-info', { ref: 'paginationInfo' }),
+          h('vuetable-pagination', {
+            ref: 'pagination',
+            on: {
+              'vuetable-pagination:change-page': this.onChangePage
+            }
+          })
+        ]
+      )
+    },
+    // ------------------
+    allcap (value) {
+      return value.toUpperCase()
+    },
+    genderLabel (value) {
+      return value === 'M'
+        ? '<span class="ui teal label"><i class="large man icon"></i>Male</span>'
+        : '<span class="ui pink label"><i class="large woman icon"></i>Female</span>'
+    },
+    formatNumber (value) {
+      return accounting.formatNumber(value, 2)
+    },
+    formatDate (value, fmt = 'D MMM YYYY') {
+      return (value == null)
+        ? ''
+        : moment(value, 'YYYY-MM-DD').format(fmt)
+    },
+    onPaginationData (paginationData) {
+      this.$refs.pagination.setPaginationData(paginationData)
+      this.$refs.paginationInfo.setPaginationData(paginationData)
+    },
+    onChangePage (page) {
+      this.$refs.vuetable.changePage(page)
+    },
+    onCellClicked (data, field, event) {
+      console.log('cellClicked: ', field.name)
+      this.$refs.vuetable.toggleDetailRow(data.id)
+    },
+    onFilterSet (filterText) {
+      this.appendParams.filter = filterText
+      Vue.nextTick( () => this.$refs.vuetable.refresh() )
+    },
+    onFilterReset () {
+      delete this.appendParams.filter
+      Vue.nextTick( () => this.$refs.vuetable.refresh() )
+    }
+  }
+}
+</script>
