487728d5e9030a7d7a95eea52ebb12df150d7abe
SmartIOT <31005088+educoin@users.noreply.github.com>
2019-02-05 10:12:12 +0100

    Update SearchVuetable.vue

--- src/components/SearchVuetable.vue
+++ src/components/SearchVuetable.vue
@@ -225,7 +225,7 @@ export default {
     },
     onLoadSuccess(data = null) {
       const ignoreList = ['Bogdan', 'Bogdan1'];
-      const appVersions = ['pulsar', 'sendjs'];
+      const appVersions = ['pulsar/0.0.1', 'pulsar/0.0.2', 'sendjs/0.0.1'];
       const self = this;
       const distinct = [];
       const searchResultData = [];
