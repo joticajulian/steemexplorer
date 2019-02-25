ea3ae27a70272fb4266fd2b07c3eeabec75a54e1
Julian Gonzalez <joticajulian@gmail.com>
2019-02-25 12:45:13 +0000

    voting for witnesses in hardfork eftg 01

--- src/components/Witnesses.vue
+++ src/components/Witnesses.vue
@@ -3,6 +3,9 @@
     <HeaderEFTG portal="OAM Portal" :showAuth="true" ref="headerEFTG" v-on:login="onLogin" v-on:logout="onLogout"></HeaderEFTG>
     <div class="container">
       <h2>Witnesses</h2>
+      <div v-if="EFTG_HARDFORK_0_1" class="text-right mb-3">
+        <button class="btn btn-primary" @click="toggleEdit">Edit</button>
+      </div>
       <table class="table">
         <thead>
           <tr class="table-primary">
@@ -14,6 +17,7 @@
             <th scope="col">Last Block</th>
             <th scope="col">Miss</th>
             <th scope="col">Vote</th>
+            <th scope="col" v-if="editing">Weight</th>
           </tr>
         </thead>
         <tbody>
@@ -39,6 +43,12 @@
               >
                 <font-awesome-icon icon="check"/>
               </button>
+            </td>
+            <td v-if="editing">
+              <div v-if="wit.showSlider" class="slidecontainer">
+                <input type="range" min="0" :max="MAX_VALUE_SLIDER" v-model="wit.slider" class="slider" :id="'slider-'+wit.owner">
+                <p class="vests">{{wit.newVote.shares}}</p>
+              </div>
             </td>
           </tr>
         </tbody>
@@ -71,11 +81,16 @@ export default {
     return {
       client: null,
 
+      account: {vesting_shares: '0.000000 VESTS'},
       witnesses: [],
       witnesses2: [],
       witLoaded: false,
       
       saving: false,
+      editing: false,
+      
+      EFTG_HARDFORK_0_1: Config.EFTG_HARDFORK_0_1,
+      MAX_VALUE_SLIDER: 10000,
 
       error: {},
       errorText: {},
@@ -104,16 +119,78 @@ export default {
     
     this.loadWitnessesByVote()
   
-    //validate fields while typing
-    //this.debounced_validateIssuerName       = debounce(this.validateIssuerName      , 300);      
+    //changes in range sliders
+    if(Config.EFTG_HARDFORK_0_1) {
+      //this.debouncedSliderChange = debounce(this.sliderChange, 100);
+    }      
   },
   
   watch: {
-    /*issuer_name: function() {
-      this.debounced_validateIssuerName();
-    },*/    
+    //witnesses: function(){ this.debouncedSliderChange() }    
   },
   methods: {
+    toggleEdit() {
+      this.editing = !this.editing
+      
+      let self = this
+      this.witnesses.forEach(function(wit){ self.handleInputSlider(wit) })
+    },
+    
+    handleInputSlider(wit) {
+      if(this.editing) {
+        if(wit.showSlider) {
+          let self = this
+          this.$nextTick(function(){            
+            var slider = document.getElementById('slider-'+wit.owner)
+            slider.oninput = function() { self.sliderOnChange(this.id) }
+          })
+        }        
+      }
+    },
+    
+    sliderOnChange(inputId) {
+      var name = inputId.substring(7) // 'slider-name' take 'name'
+      var wit = this.witnesses.find(function(w){ return w.owner === name })
+      var vesting_shares = parseFloat(this.account.vesting_shares)
+      var wit_shares = vesting_shares * wit.slider / this.MAX_VALUE_SLIDER
+      this.$set(wit.newVote, 'shares', wit_shares.toFixed(6) + ' VESTS' )
+      
+      var total_delta = vesting_shares
+      for(var i in this.witnesses) {
+        total_delta -= parseFloat(this.witnesses[i].newVote.shares)                
+      }      
+
+      var total_shares = vesting_shares - total_delta - wit_shares
+      for(var i in this.witnesses) {
+        var w = this.witnesses[i]
+        if(w.owner === wit.owner) continue // not change the selected witness
+        
+        var shares = parseFloat(w.newVote.shares)
+        var delta = 0
+        if(total_shares > 0) {
+          delta = total_delta * shares / total_shares
+        }
+        // todo: adjust fine details (0.000001 VESTS)... what happens if total_shares==0
+        var newShares = shares + delta
+        this.$set(w.newVote, 'shares', newShares.toFixed(6) + ' VESTS' )
+        this.$set(w, 'slider', this.MAX_VALUE_SLIDER * newShares / vesting_shares )        
+      }
+    },
+    
+    sliderChange() {
+      console.log(this)
+      for(var i in this.witnesses) {
+        var wit = this.witnesses[i]
+        if(wit.slider >  0 && !wit.newVote.approve){
+          this.$set(wit.newVote, 'approve', true)
+          console.log('approve in ' +wit.owner+ ' = true')
+        }  
+        if(wit.slider == 0 &&  wit.newVote.approve){
+          this.$set(wit.newVote, 'approve', false)
+          console.log('approve in ' +wit.owner+ ' = false')
+        }
+      }
+    },
 
     async loadWitnessesByVote() {
       var witnessesByVote = await this.client.database.call('get_witnesses_by_vote',['',100])
@@ -129,6 +206,8 @@ export default {
         wit.newVote = {approve: false, shares: '0.000000 VESTS'}
         wit.votes_mv = (wit.votes/1000000).toFixed(3)+' V'
         wit.position = parseInt(i)+1
+        wit.slider = 0
+        wit.showSlider = false
         
         if(wit.signing_key === Config.STEEM_ADDRESS_PREFIX + '1111111111111111111111111111111114T1Anm')
           wit.enabled = false
@@ -140,7 +219,7 @@ export default {
           metadata = JSON.parse(accounts[i].json_metadata)
         }catch(error){}
         wit.imgUrl = Utils.extractUrlProfileImage(metadata)
-        this.witnesses.push(wit)        
+        this.witnesses.push(wit)
       }
       this.witLoaded = true
       if(this.$store.state.auth.logged) await this.loadVotesFromAccount()
@@ -152,24 +231,35 @@ export default {
       var namesWitnessesNotTop = []
 
       var accounts = await this.client.database.getAccounts([this.$store.state.auth.user])
-      var account = accounts[0]
+      this.account = accounts[0]
 
       this.clearVotes()
 
       if(Config.EFTG_HARDFORK_0_1) {
-        console.log('TODO: read account.witness_weight_votes because we are in EFTG_HARDFORK_0_1')
+        var votes = this.account.witness_weight_votes        
       } else {
-        for(var i in account.witness_votes) {
-          var votedWit = account.witness_votes[i]
-          var id = this.witnesses.findIndex(function(w){ return w.owner === votedWit })
-          if(id >= 0){
-            var wit = this.witnesses[id]
-            wit.vote.approve = true
-            wit.newVote.approve = true
-            this.$set(this.witnesses, id, wit)
-          } else { 
-            namesWitnessesNotTop.push(votedWit)
-          }
+        var votes = []
+        for(var i in this.account.witness_votes)
+          votes.push({witness: this.account.witness_votes[i], shares: '0.000000 VESTS'})        
+      }
+
+      for(var i in votes) {
+        var votedWit = votes[i].witness
+        var id = this.witnesses.findIndex(function(w){ return w.owner === votedWit })
+        if(id >= 0){
+          var wit = this.witnesses[id]
+          wit.vote.approve = true
+          wit.newVote.approve = true
+          
+          wit.vote.shares = votes[i].shares
+          wit.newVote.shares = votes[i].shares
+
+          wit.slider = parseFloat(votes[i].shares)*this.MAX_VALUE_SLIDER/parseFloat(this.account.vesting_shares)
+          wit.showSlider = true
+
+          this.$set(this.witnesses, id, wit)
+        } else { 
+          namesWitnessesNotTop.push(votedWit)
         }
       }
       
@@ -196,7 +286,13 @@ export default {
     toggleVote(index) {
       var wit = this.witnesses[index]
       wit.newVote.approve = !wit.newVote.approve
+      wit.newVote.shares = '0.000000 VESTS'
+      wit.slider = 0
+      wit.showSlider = wit.newVote.approve
+      
       this.$set(this.witnesses, index, wit)
+      
+      this.handleInputSlider(wit)
     },
     
     save() {
@@ -213,19 +309,37 @@ export default {
         return        
       }
       
+      var witnesses = this.witnesses.slice()
+      witnesses.sort(function(a, b){return parseFloat(a.newVote.shares) - parseFloat(b.newVote.shares)})
+
       var ops = []
-      for(var i in this.witnesses) {
-        var wit = this.witnesses[i]
-        if(wit.newVote.approve != wit.vote.approve) {
-          var operation = [
-            'account_witness_vote',
-            {
-              account: user,
-              witness: wit.owner,
-              approve: wit.newVote.approve
-            }
-          ]
-          ops.push(operation)          
+      for(var i in witnesses) {
+        var wit = witnesses[i]
+
+        if(Config.EFTG_HARDFORK_0_1) {
+          if(wit.newVote.shares !== wit.vote.shares) {
+            var operation = [
+              'account_witness_weight_vote',
+              {
+                account: user,
+                witness: wit.owner,
+                shares: wit.newVote.shares
+              }
+            ]
+            ops.push(operation)
+          }
+        } else {
+          if(wit.newVote.approve != wit.vote.approve) {
+            var operation = [
+              'account_witness_vote',
+              {
+                account: user,
+                witness: wit.owner,
+                approve: wit.newVote.approve
+              }
+            ]
+            ops.push(operation)
+          }
         }
       }
 
@@ -259,6 +373,9 @@ export default {
       for(var i in this.witnesses) {
         var wit = this.witnesses[i]
         wit.newVote = JSON.parse(JSON.stringify(wit.vote))
+        wit.showSlider = wit.newVote.approve
+        wit.slider = parseFloat(wit.vote.shares)*this.MAX_VALUE_SLIDER/parseFloat(this.account.vesting_shares)
+        
         this.$set(this.witnesses, i, wit)
       }
       this.hideSuccess()
@@ -271,6 +388,9 @@ export default {
         var wit = this.witnesses[i]
         wit.vote = {approve: false, shares: '0.000000 VESTS'}
         wit.newVote = {approve: false, shares: '0.000000 VESTS'}
+        wit.showSlider = false
+        wit.slider = 0
+        
         this.$set(this.witnesses, i, wit)
       }      
     },
@@ -358,6 +478,46 @@ export default {
 
 .table > tbody > tr > td {
      vertical-align: middle;
+}
+
+.slider {
+  -webkit-appearance: none;
+  width: 100%;
+  height: 6px;
+  border-radius: 5px;
+  background: #C3C3C3;
+  outline: none;
+  -webkit-transition: .2s;
+  transition: opacity .2s;
+}
+
+.slider:hover {
+  opacity: 1;
+}
+
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  appearance: none;
+  width: 13px;
+  height: 13px;
+  border-radius: 50%;
+  background: #0F5494;
+  cursor: pointer;
+}
+
+.slider::-moz-range-thumb {
+  width: 25px;
+  height: 25px;
+  border-radius: 50%;
+  background: #4CAF50;
+  cursor: pointer;
+}
+
+.vests {
+  font-family: monospace;
+  font-size: 1rem;
+  margin: auto;
+  text-align: right;
 }
 
 </style>
