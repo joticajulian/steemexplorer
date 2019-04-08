278322fd82f91a1e6ad7c5112465aa00824a4dd5
Pablo Staiano <1355981+pablomat@users.noreply.github.com>
2019-04-08 13:43:51 +0200

    Added new RPC node from Italy

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ const config = {
   'production': {
     RPC_NODES : [
       'https://api.eftg.eu',
+      'https://rpc-italy.eftg.eu',
       'https://rpc-germany.eftg.eu',
       'https://rpc-luxembourg.eftg.eu',
       'https://rpc-romania.eftg.eu'
