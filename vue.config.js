243d66437a57bebe5594f988c5c9f9970f196cf6
Julian Gonzalez <joticajulian@gmail.com>
2019-04-16 09:18:16 +0000

    instructions to install webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  devServer: { disableHostCheck: true }
-  }
+  devServer: { disableHostCheck: true },
+
+  /** Bundle Analyzer
+   *
+   *  to intall:
+   *  vue add webpack-bundle-analyzer
+   */
+  /* pluginOptions: {
+    webpackBundleAnalyzer: {
+      openAnalyzer: true,
+      analyzerHost: '0.0.0.0',
+      analyzerPort: '8084',
+    }
+  }*/
+}
