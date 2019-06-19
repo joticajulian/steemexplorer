31a66b1d0bf93fda97bc9804dc3e4fbf13026d6a
Julian Gonzalez <joticajulian@gmail.com>
2019-06-19 07:19:27 +0000

    header component: new links, css, and logout

--- src/components/HeaderEFTG.vue
+++ src/components/HeaderEFTG.vue
@@ -34,10 +34,22 @@
       <b-collapse id="nav-collapse" is-nav>
         <ul class="navbar-nav">
           <li class="nav-item">
+            <router-link to="/adminstudents" class="nav-item nav-link"><div class="extra-padding">Admin</div></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/courses" class="nav-item nav-link"><div class="extra-padding">Courses</div></router-link>
+          </li>
+          <li class="nav-item">
             <router-link to="/issue" class="nav-item nav-link"><div class="extra-padding">Issue Credentials</div></router-link>
           </li>
           <li class="nav-item">
             <router-link to="/keymanagement" class="nav-item nav-link"><div class="extra-padding">Key management</div></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/proof" class="nav-item nav-link"><div class="extra-padding">Proof</div></router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/verify" class="nav-item nav-link"><div class="extra-padding">Verify</div></router-link>
           </li>
           <li class="nav-item">
             <router-link to="/explorer" class="nav-item nav-link"><div class="extra-padding">Explorer</div></router-link>
@@ -82,6 +94,7 @@
 <script>
 import AuthComponent from '@/components/Auth'
 import Config from '@/config.js'
+import axios from 'axios'
 
 export default {
   name: "HeaderEFTG",
@@ -118,8 +131,9 @@ export default {
       this.$refs.modalAuth.hide()    
     },
     
-    logout() {
-      console.log("@" + this.$store.state.auth.user + " logout");
+    async logout() {
+      await axios.get(Config.SERVER_API + "logout")
+      console.log(this.$store.state.auth.username + " logout");
       this.$store.state.auth = {
         user: "",
         logged: false,
@@ -143,7 +157,7 @@ export default {
 <style scoped>
 
 #logo {
-  height: 4rem;
+  height: 2.3rem;
 }
 
 #logo img {
