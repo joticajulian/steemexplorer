cdea6f61b10aa68aa9d72044e65b7ba971a8a8d2
Julian Gonzalez <joticajulian@gmail.com>
2019-06-28 14:51:10 +0000

    env for development

--- src/mixins/Auth.js
+++ src/mixins/Auth.js
@@ -12,14 +12,26 @@ export default {
   methods: {
 
     async login(username, password) {
+      if(process.env.VUE_APP_DEV){
+        var dev_user = { 
+          username: process.env.VUE_APP_DEV_USERNAME,
+          imgUrl: "'https://steemitimages.com/DQmb2HNSGKN3pakguJ4ChCRjgkVuDN9WniFRPmrxoJ4sjR4'",
+          logged: true
+        }
+        this.$store.state.auth = dev_user
+        return dev_user
+      }
+
+      var response
+      var user
       if(!username && !password){
-        var response = await axios.get(Config.SERVER_API + 'login')
-        var user = this.setLoginUser(response.data)
+        response = await axios.get(Config.SERVER_API + 'login')
+        user = this.setLoginUser(response.data)
         return user
       }
 
-      var response = await axios.post(Config.SERVER_API + "login", {username, password})
-      var user = this.setLoginUser(response.data)
+      response = await axios.post(Config.SERVER_API + "login", {username, password})
+      user = this.setLoginUser(response.data)
 
       router.push(Config.PAGE_AFTER_LOGIN)
       return user
