02e2365415ffb651724deee022c394bfba7e5d69
Julian Gonzalez <joticajulian@gmail.com>
2019-07-03 09:10:17 +0000

    router no history mode, public path ./

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: { disableHostCheck: true },
 
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/steemexplorer/'
+    ? './'
     : '/'
   /** Bundle Analyzer
    *
