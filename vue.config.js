9afed1d0469fb175c5af8bf82bfaacd48bb74b3c
Julian Gonzalez <joticajulian@gmail.com>
2019-07-03 09:02:26 +0000

    router history mode. change publicpath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   devServer: { disableHostCheck: true },
 
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/steemexplorer/'
+    : '/'
   /** Bundle Analyzer
    *
    *  to intall:
