d2d3945292df16ebe06295f5b03dd91b8a7bf12f
Julian <joticajulian@gmail.com>
2019-12-28 01:57:25 +0100

    select distribution (freq or cumulative). Show axis labels

--- src/views/ReportsPage.vue
+++ src/views/ReportsPage.vue
@@ -18,17 +18,23 @@
           <div>Budget for rewards: {{dayprops.budget}} STEEM</div>
           <div>Total rewards for posts: {{dayprops.reward_posts}} STEEM ({{dayprops.perc_reward_posts}}%)</div>
           <div>Total rewards for comments: {{dayprops.reward_comments}} STEEM ({{dayprops.perc_reward_comments}}%)</div>
-          <div>Median payout: {{dayprops.median_payout}} SBD</div>
+          <div>Median payout: {{dayprops.median_payout}} STEEM ({{dayprops.median_payout_sbd}} SBD)</div>
+          <div class="text-right">
+            <select v-model="sel_distribution">
+              <option value="frequency">Frequency distribution</option>
+              <option value="cumulative">Cumulative distribution</option>
+            </select>
+          </div>
           <div class="row">
             <div class="col-md-6">
-          <BarChart :chartData="chartPostSBD" :options="options"></BarChart>
-          <BarChart :chartData="chartPostFreq" :options="options"></BarChart>
+              <BarChart :chartData="chartPostSBD.chartData" :options="chartPostSBD.options"></BarChart>
+              <BarChart :chartData="chartPostFreq.chartData" :options="chartPostFreq.options"></BarChart>
+            </div>
+            <div class="col-md-6">
+              <BarChart :chartData="chartCommentSBD.chartData" :options="chartCommentSBD.options"></BarChart>
+              <BarChart :chartData="chartCommentFreq.chartData" :options="chartCommentFreq.options"></BarChart>
+            </div>
           </div>
-<div class="col-md-6">
-          <BarChart :chartData="chartCommentSBD" :options="options"></BarChart>
-          <BarChart :chartData="chartCommentFreq" :options="options"></BarChart>
-          </div>
-      </div>
         </div>
       </div>
     </div>    
@@ -52,13 +58,20 @@ export default {
       there_are_more_days: false,
       days: [],
       db: null,
+      sel_distribution: 'frequency',
       chartPostSBD: { labels: [], datasets: [] },
       chartPostFreq: { labels: [], datasets: [] },
+      chartPostAccum: { labels: [], datasets: [] },
       chartCommentSBD: { labels: [], datasets: [] },
       chartCommentFreq: { labels: [], datasets: [] },
+      chartCommentAccum: { labels: [], datasets: [] },
       options: {
         responsive: true,
         maintainAspectRatio: false
+      },
+      curDay: {
+        data: null,
+        gprops: null,
       },
       dayprops: {
         budget: 0,
@@ -66,7 +79,8 @@ export default {
         reward_comments: 0,
         perc_reward_posts: 0,
         perc_reward_comments: 0,
-        median_payout: 0
+        median_payout: 0,
+        median_payout_sbd: 0
       },
     }
   },
@@ -105,6 +119,9 @@ export default {
   },
 
   watch: {
+    sel_distribution: function(){
+      this.drawCharts()
+    }
   },
   
   methods: {
@@ -133,7 +150,8 @@ export default {
         var div = data.post.total_claims / data.post.total_rshares
         var mrs = 2*s * (2*div - 1)/(1 - div)  // median rshares
         var mcl = mrs * (mrs + 2*s) / (mrs + 4*s) // median claims
-        this.dayprops.median_payout = (mcl * gprops.reward_balance / gprops.recent_claims * gprops.feed_price).toFixed(3)
+        this.dayprops.median_payout = (mcl * gprops.reward_balance / gprops.recent_claims).toFixed(3)
+        this.dayprops.median_payout_sbd = (mcl * gprops.reward_balance / gprops.recent_claims * gprops.feed_price).toFixed(3)
         gprops.mrs = mrs
 
         this.dayprops.budget = Math.round(gprops.reward_balance / 15 * 1000)/1000
@@ -142,38 +160,62 @@ export default {
         this.dayprops.perc_reward_posts = Math.round(this.dayprops.reward_posts / this.dayprops.budget * 100 * 100)/100
         this.dayprops.perc_reward_comments = Math.round(this.dayprops.reward_comments / this.dayprops.budget * 100 * 100)/100
 
-        this.chartPostSBD = this.generateChart(data.post,
-          { gprops:gprops,
-            dist:'sbd', 
-            backgroundColor:'#3c8dbc',
-            backgroundColorMedian:'#0546b5',
-            title:'Paid SBD - Posts'
-          })
-        this.chartPostFreq = this.generateChart(data.post,
-          { gprops:gprops,
-            dist:'freq',
-            backgroundColor:'#3c8dbc',
-            backgroundColorMedian:'#0546b5',
-            title: 'Number of posts'
-          })
-        this.chartCommentSBD = this.generateChart(data.comment,
-          { gprops:gprops,
-            dist:'sbd',
-            backgroundColor:'#3c8dbc',
-            backgroundColorMedian:'#0546b5',
-            title: 'Paid SBD - Comments'
-          })
-        this.chartCommentFreq = this.generateChart(data.comment,
-          { gprops:gprops,
-            dist:'freq',
-            backgroundColor:'#3c8dbc',
-            backgroundColorMedian:'#0546b5',
-            title: 'Number of comments'
-          })
+        this.curDay = {
+          data,
+          gprops,
+        }
+        this.drawCharts()
       }catch(error){
         console.log("Error getting document:", error)
       }
     },
+
+    drawCharts(){
+      var data = this.curDay.data
+      var gprops = this.curDay.gprops
+      var cumulative = this.sel_distribution === 'cumulative' ? true : false
+      this.chartPostSBD = this.generateChart(data.post,
+        { gprops:gprops,
+          dist:'sbd',
+          cumulative,
+          backgroundColor:'#3c8dbc',
+          backgroundColorMedian:'#0546b5',
+          title: cumulative ? 'Cumulative distribution of post rewards' : 'Distribution of post rewards',
+          ytitle: cumulative ? 'Percentage' : 'Total rewards [STEEM]',
+          xtitle: 'Post value [STEEM]'
+        })
+      this.chartPostFreq = this.generateChart(data.post,
+        { gprops:gprops,
+          dist:'freq',
+          cumulative,
+          backgroundColor:'#3c8dbc',
+          backgroundColorMedian:'#0546b5',
+          title: cumulative ? 'Cumulative distribution of number of posts' : 'Number of posts',
+          ytitle: cumulative ? 'Percentage' : 'Number of posts',
+          xtitle: 'Post value [STEEM]'
+        })
+      this.chartCommentSBD = this.generateChart(data.comment,
+        { gprops:gprops,
+          dist:'sbd',
+          cumulative,
+          backgroundColor:'#3c8dbc',
+          backgroundColorMedian:'#0546b5',
+          title: cumulative ? 'Cumulative distribution of comment rewards' : 'Distribution of comment rewards',
+          ytitle: cumulative ? 'Percentage' : 'Total rewards [STEEM]',
+          xtitle: 'Post value [STEEM]'
+        })
+      this.chartCommentFreq = this.generateChart(data.comment,
+        { gprops:gprops,
+          dist:'freq',
+          cumulative,
+          backgroundColor:'#3c8dbc',
+          backgroundColorMedian:'#0546b5',
+          title: cumulative ? 'Cumulative distribution of number of comments' : 'Number of comments',
+          ytitle: cumulative ? 'Percentage' : 'Number of comments',
+          xtitle: 'Post value [STEEM]'
+        })
+    },
+
     generateChart(data,opts) {
       var gprops = opts.gprops
       var s = 2e12
@@ -185,17 +227,25 @@ export default {
         var rs = data.x[i]
         var claims = rs * (rs + 2*s) / (rs + 4*s)
         var steem = gprops.reward_balance / gprops.recent_claims * claims
-        var sbd = steem * gprops.feed_price
-        x[i] = '$ ' + sbd.toFixed(3)
+        // var sbd = steem * gprops.feed_price
+        x[i] = steem.toFixed(3)
         switch(opts.dist){
           case 'sbd':
-            y[i] = Math.round(data.y[i] * sbd * 100)/100
+            y[i] = Math.round(data.y[i] * steem * 100)/100
             break
           case 'freq':
             y[i] = data.y[i]
             break
           default:
             throw new Error('Error in generatechart')
+        }
+
+        if(opts.cumulative){
+          var delta = y[i]
+          if(i==0)
+            y[i] = delta
+          else
+            y[i] = y[i-1] + delta
         }
 
         if(!median_bar_done && rs > gprops.mrs){
@@ -206,13 +256,48 @@ export default {
         }
       }
 
+      if(opts.cumulative){
+        var total_y = y[y.length-1]
+        for(i in y){
+          y[i] = y[i] / total_y * 100
+        }
+      }
+
       return {
-        labels: x,
-        datasets:[{
-          label: opts.title,
-          backgroundColor: bgc,
-          data: y
-        }]
+        chartData:{
+          labels: x,
+          datasets:[{
+            label: opts.title,
+            backgroundColor: bgc,
+            data: y
+          }],
+        },
+        options: {
+          responsive: true,
+          maintainAspectRatio: false,
+          scales: {
+            yAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: opts.ytitle
+              },
+              ticks: {
+                callback: function(value){
+                  if(opts.cumulative)
+                    return value + '%'
+                  else
+                    return value
+                }
+              }
+            }],
+            xAxes: [{
+              scaleLabel: {
+                display: true,
+                labelString: opts.xtitle
+              }
+            }]
+          }
+        }
       }
     },
     loadMoreDays(){},
